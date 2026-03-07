e82b4334ad915047e360ec906939b04806dd7653
Ben Braun <benjamin.braun2003@gmail.com>
2026-03-07 01:35:56 -0500

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Greeting from './Components/Greeting'
+
+function App() {
+
+	return (
+		<Greeting />
+
+	)
+}
+
+export default App
---
+++ src/Components/Greeting.jsx
@@ -0,0 +1,11 @@
+
+function Greeting() {
+
+
+	return (
+		<h1>Hello React</h1>
+	)
+
+}
+
+export default Greeting
