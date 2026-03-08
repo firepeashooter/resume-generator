1978b353811c3fb9de89f00e1db19380961fc801
Ben Braun <benjamin.braun2003@gmail.com>
2026-03-08 15:38:39 -0400

    Styling subsections

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -11,9 +11,9 @@ function Resume() {
 
 				<h2 className="name">Benjamin Braun</h2>
 				<div className="info">
-					<span className="contact--info">(226-224-9308)</span>
-					<span className="contact--info">benjamin.braun2003@gmail.com</span>
-					<span className="contact--info">Kingston Jamaica</span>
+					<span>(226-224-9308)</span>
+					<span>benjamin.braun2003@gmail.com</span>
+					<span>Kingston Jamaica</span>
 				</div>
 
 			</div>
@@ -22,7 +22,38 @@ function Resume() {
 
 				<h3 className="resume--sub--header">Education</h3>
 
+				<div className="sub--container">
+					<div className="date--info">
+
+						<span>
+							DateOne - DateTwo
+						</span>
+						<span>
+							Kingston Jamaica
+						</span>
+
+					</div>
+					<div className="sub--info">
+						<h4>Company Seven</h4>
+						<span>Junior Company Developer</span>
+						<p className="info--paragraph">
+							This is the information about the company Here is a lot more information about the
+							company to see how it will look
+
+							this is a great idea
+						</p>
+
+					</div>
+				</div>
+
 			</div>
+			<div className="sub--section">
+
+				<h3 className="resume--sub--header">Experience</h3>
+
+			</div>
+
+
 
 		</div>
 	)
