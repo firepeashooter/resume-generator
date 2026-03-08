d37bcb573ac82bb8f8ea738119fdd001c7c320a9
Ben Braun <benjamin.braun2003@gmail.com>
2026-03-08 14:59:39 -0400

    Rework Header and Setup Education Section

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -9,6 +9,19 @@ function Resume() {
 
 			<div className="resume--header">
 
+				<h2 className="name">Benjamin Braun</h2>
+				<div className="info">
+					<span className="contact--info">(226-224-9308)</span>
+					<span className="contact--info">benjamin.braun2003@gmail.com</span>
+					<span className="contact--info">Kingston Jamaica</span>
+				</div>
+
+			</div>
+
+			<div className="sub--section">
+
+				<h3 className="resume--sub--header">Education</h3>
+
 			</div>
 
 		</div>
