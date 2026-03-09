0ecff165ef5b9e88cfb401ba5590a5ebeea0c043
Ben Braun <benjamin.braun2003@gmail.com>
2026-03-09 00:09:07 -0400

    Create generic InputBox Component

---
+++ src/Components/InputBox.jsx
@@ -0,0 +1,18 @@
+
+//TODO: Make this take a props so we can make generic input boxes
+function InputBox() {
+
+	return (
+
+		<div className="input--box">
+
+			<label for="player1">First Name</label>
+			<input type="text" name="player1" />
+
+		</div>
+
+
+	)
+}
+
+export default InputBox
